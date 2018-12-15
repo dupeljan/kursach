aaac10fbc097f3cd35a89b78a6996c23349dd805
Dupeljan Linux <dupeljan@gmail.com>
2018-12-15 15:44:51 +0300

    Filter modif

--- filter.py
+++ filter.py
@@ -3,10 +3,10 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
-SOURCE_PATCH = "test5"
-RESULT_PATH = "filtred5"
+SOURCE_PATCH = "noise test5"
+RESULT_PATH = "filtred5 noise"
+LOG_FILE = "errors " + SOURCE_PATCH
 
-ERROR_COUNT = 0
 
 def filter(name):
 	#name = '1'
@@ -20,14 +20,14 @@ def filter(name):
 	If average more than 128 => number is green.
 	Otherwise is red
 	'''
-	print ("A ",average_a,  name)
+	#print ("A ",average_a,  name)
 	a = cv2.split(lab)[1]
 	if average_a < 130:
 		rgb = cv2.threshold(a,132,255,cv2.THRESH_BINARY)[1]
 		state = 1
 	else:
 		average_b = np.array( list(x[2]  for i in range(len(lab)) for x in lab[i] )) .mean()
-		print("B ",average_b, name)
+		#print("B ",average_b, name)
 		if  average_b < 135: #130 134
 			state = 2
 			rgb = cv2.threshold(a,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]#'''average'''
@@ -35,12 +35,12 @@ def filter(name):
 			state = 3
 			rgb = cv2.threshold(cv2.split(rgb)[2],227,255,cv2.THRESH_BINARY)[1] # Red chanel
 			rgb = cv2.bitwise_not(rgb)
-
-	print("State",state)
+	error = False
 	if ( name[-1] != str(state)):
-		ERROR_COUNT += 1
-		file = open("error", 'a' )
-		file.write("a ",average_a," b ",average_b," "name)
+		error = True
+		file = open(LOG_FILE, 'a' )
+		file.write("a "+ str(average_a) + " b " + str(average_b) + " " + name + "\n"\
+					"Got type " + str(state) +"\n")
 		file.close()
 	#fig = plt.hist(a.ravel(),256)	
 	#plt.savefig("hist"+name+".png")
@@ -76,16 +76,28 @@ def filter(name):
 	'''
 	dst = cv2.medianBlur(closing,5)
 
-	cv2.imwrite( os.path.join(RESULT_PATH ,  name + " res"+".png"),dst )
+	cv2.imwrite( os.path.join(RESULT_PATH ,  name +".png"),dst )
+	print(name+".png filtered")
+
+	return error
 
 def main():
+	if os.path.isfile(LOG_FILE):
+		os.remove(LOG_FILE)
 	if not os.path.isdir(RESULT_PATH):
 		os.mkdir(RESULT_PATH)
 	files = os.listdir(SOURCE_PATCH)
+	errors = 0
 	for filename in files:
 		name,ext = os.path.splitext(filename)
 		if ext == ".png":
-			filter(name)
+			if (filter(name)):
+				errors += 1
+	file = open(LOG_FILE, 'a' )
+	file.write("Error counts: " + str(errors) + '\n')
+	file.write("% " + str(errors / float(len(files)) ) + '\n')
+	file.write("#"*40)
+	file.close()
 
 if __name__ == '__main__':
 	main()
