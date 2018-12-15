328950559fd46622d27ea560d03baa1811b00129
Dupeljan Linux <dupeljan@gmail.com>
2018-12-15 22:49:15 +0300

    Add the beddest nnetwork

---
+++ nnetwork.py
@@ -0,0 +1,78 @@
+import numpy as np
+import scipy.special
+from math import *
+
+class Neural_network:
+	"""docstring for Neural_network"""
+	def __init__(self, layers_shape, speed= 0.5, alpha= 1 ):
+		# Learning speed
+		self.speed = speed
+
+		self.alpha = alpha
+
+		self.shape = layers_shape
+
+		self.activation_function = lambda x: 1 / ( 1 - exp( -2 * alpha * x ))
+		# Tensor of output all neurons
+		self.output = np.ndarray(shape= (len(layers_shape) - 1, ) , dtype= list)
+		# Init weight
+		self.weights_list = list()
+		for i in range(len(layers_shape)-1):
+			self.weights_list.append( np.random.normal(0.0, pow(layers_shape[i], -0.5), (layers_shape[i], layers_shape[i+1])))
+
+	def sign(self,input):
+		return [ self.activation_function(x) for x in input ]
+	
+	def predict(self,input, i= 0):
+		y = self.sign( np.dot(input,self.weights_list[i]) )
+		if len(self.weights_list) == i + 1 :
+			return y
+		return self.predict(y,i+1)
+
+	def get_output(self,input, i= 0):
+		'''
+		Return output matrix
+		'''
+		y = self.sign( np.dot(input,self.weights_list[i]) )
+		self.output[i] = y 
+		if len(self.weights_list) == i + 1 :
+			self.output[0] = input
+			return self.output
+		return self.get_output(y,i+1)
+
+
+	
+	def lern(self,input,label):
+		output = self.get_output(input)
+		delta = np.ndarray(shape= (len(self.weights_list) , max(self.shape) ) , dtype= list)
+		# DESCENT
+		# LAST LAYER
+		for i, x in enumerate(output[-1]):
+			delta[-1][i] = -2 * self.alpha * x * ( 1 - x ) * ( label[i] - x )
+		# HIDEN LAYERS
+		# REVERSE, BEGIN FROM SECOND FROM END
+		for i, local_out in enumerate(output[1::-1]): # CHOOSE LAYER
+			for j, x in enumerate(local_out): # CHOOSE NEURON
+
+				delta[i][j] = 2 * self.alpha * ( 1 - x ) * sum ( delta[i+1][k] * self.weights_list[i+1][j][k] for k in range( len( delta[i+1] ) ) ) 
+		# UPDATE WEIGHTS
+		for i in len(range(self.weights_list)): # CHOOSE LAYER
+			for j in len(range(self.weights_list[i])): # CHOOSE ROW 
+				for k in len(range(self.weights_list[i][j])): # CHOOSE COLUMN
+					weights_list[i][j][k] -= self.speed * delta[i][k] * output[i][j] 
+
+
+
+
+
+
+def main():
+	nn = Neural_network([5,5,1])
+	nn.lern([1,2,3,4,5],[0])
+	print ( nn.predict([1,2,3,4,5]) )
+
+if __name__ == '__main__':
+	main()
+
+
+		
